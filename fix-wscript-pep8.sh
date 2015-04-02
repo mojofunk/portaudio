4a820c4e0e5f0975123470d03037c7c6099b9d6e
Tim Mayberry <mojofunk@gmail.com>
2015-04-02 23:17:51 +1000

    Add script to reformat wscript file to pep8 standard

---
+++ fix-wscript-pep8.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+if hash pep8 2>/dev/null; then
+    find ./ -type f -name "wscript" | xargs autopep8 -i -a
+else
+    echo "autopep8 is not installed, cannot fix wscript files for pep8 conformity"
+	echo "try $ sudo pip install autopep8"
+fi
