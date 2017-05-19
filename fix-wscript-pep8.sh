fe3e5bb5c8e17408f1a9e7d6c9ae1a1f6a95c420
Tim Mayberry <mojofunk@gmail.com>
2017-05-19 14:57:00 +1000

    Fix script to format to pep8 standards

--- fix-wscript-pep8.sh
+++ fix-wscript-pep8.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if hash pep8 2>/dev/null; then
+if hash autopep8 2>/dev/null; then
     find ./ -type f -name "wscript" | xargs autopep8 -i -a
 else
     echo "autopep8 is not installed, cannot fix wscript files for pep8 conformity"
