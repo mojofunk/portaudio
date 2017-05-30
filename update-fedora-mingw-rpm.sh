a4abd140cf6dcafd2ebfd05fe1eb35817259f184
Tim Mayberry <mojofunk@gmail.com>
2017-05-30 17:18:49 +1000

    Update fedora mingw script to use wafer

--- update-fedora-mingw-rpm.sh
+++ update-fedora-mingw-rpm.sh
@@ -2,8 +2,5 @@
 
 FEDORA_MINGW_ARDOUR="../fedora-mingw-ardour"
 
-cp waf "$FEDORA_MINGW_ARDOUR/SOURCES/portaudio-2.0-waf" || exit 1
-cp wscript "$FEDORA_MINGW_ARDOUR/SOURCES/portaudio-2.0-wscript" || exit 1
-
-./waf dist || exit 1
+./wafer dist || exit 1
 cp portaudio-2.0.tar.bz2 "$FEDORA_MINGW_ARDOUR/SOURCES" || exit 1
